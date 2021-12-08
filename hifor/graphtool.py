08708dd8e420ddd15018ee3becc9c6817da22e75
WenyinWei <wenyin.wei.ww@gmail.com>
2021-12-08 11:37:46 +0800

    prepend var name by '_'

--- hifor/graphtool.py
+++ hifor/graphtool.py
@@ -12,45 +12,48 @@ def add_nodes_and_edges_from_Eq(graph, eq):
 def is_single_source_DAG(graph):
     if not is_directed_acyclic_graph(graph):
         raise ValueError("The graph has to be a directed acyclic graph first before it is a single-source DAG.")
-    source_num = 0
+    _source_num = 0
     for n in graph.nodes:
         if graph.in_degree(n) == 0:
-            source_num += 1
+            _source_num += 1
         if graph.in_degree(n) + graph.out_degree(n) == 0:
             raise ValueError("There exists an isolated node in the graph, we do not check the single-source property of such a graph.")
-    if source_num == 1:
+    if _source_num == 1:
         return True
     else:
         return False
 def source_set_of_DAG(graph):
-    sources = set()
+    _sources = set()
     for n in graph.nodes:
         if graph.in_degree(n) == 0:
-            sources.add(n)
-    return sources
+            _sources.add(n)
+    return _sources
 
 def is_single_sink_DAG(graph):
     if not is_directed_acyclic_graph(graph):
         raise ValueError("The graph has to be a directed acyclic graph first before it is a single-sink DAG.")
-    sink_num = 0
+    _sink_num = 0
     for n in graph.nodes:
         if graph.out_degree(n) == 0:
-            sink_num += 1
+            _sink_num += 1
         if graph.in_degree(n) + graph.out_degree(n) == 0:
             raise ValueError("There exists an isolated node in the graph, we do not check the single-sink property of such a graph.")
-    if sink_num == 1:
+    if _sink_num == 1:
         return True
     else:
         return False
 def sink_set_of_DAG(graph):
-    sinks = set()
+    _sinks = set()
     for n in graph.nodes:
         if graph.out_degree(n) == 0:
-            sinks.add(n)
-    return sinks
+            _sinks.add(n)
+    return _sinks
 
 def lambdify_graph(graph):
     if not is_single_source_DAG(graph):
         raise ValueError("The graph to be lambdified must be a single source directed acyclic graph.")
+    else:
+        _source_set = source_set_of_DAG(graph)
+    _source_set = 
     
     return 
